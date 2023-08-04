01222bff660d62344691a2f716cccd09ecfbfe47
devtech5789 <devtech5789@gmail.com>
2023-08-04 18:55:11 +0200

    NextJS programming

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Home = () => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		console.log("Placing your order");
+		router.push("/product");
+	};
+
+	return (
+		<>
+			<h1>Home Page</h1>
+			<Link href="/blog">
+				<a>Blog</a>
+			</Link>
+			<Link href="/product">
+				<a>Product</a>
+			</Link>
+			<button onClick={handleClick}>Place order</button>
+		</>
+	);
+};
+
+export default Home;
---
+++ pages/product/index.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const ProductList = ({ productId = 100 }) => {
+	return (
+		<>
+			<Link href="/">
+				<a>Home</a>
+			</Link>
+			<h1>
+				<Link href="/product/1">
+					<a>Product 1</a>
+				</Link>
+			</h1>
+			<h1>
+				<Link href="/product/2" replace>
+					<a>Product 2</a>
+				</Link>
+			</h1>
+			<h1>
+				<Link href={`/product/${productId}`}>
+					<a>Product 3</a>
+				</Link>
+			</h1>
+		</>
+	);
+};
+
+export default ProductList;
